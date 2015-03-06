57e33f9e6f90240876245802014c865ef5ea01c4
Jakob Weisblat <jakob@weisbl.at>
2015-03-06 14:32:53 -0500

    PEP 8 Refactor on models.py

--- mafia/mafia/models.py
+++ mafia/mafia/models.py
@@ -177,7 +177,7 @@ class Player(models.Model):
         :return: False if the rogue can kill now, otherwise the next kill day
         """
         next_kill_day = self.role_information
-        if self.game.current_day >= self.role_information:
+        if self.game.current_day >= next_kill_day:
             if not self.kills.exists():
                 return False
             else:
@@ -189,7 +189,7 @@ class Player(models.Model):
                 else:
                     return False
 
-    rogue_cant_kill = property(cant_rogue_kill)
+    next_rogue_kill_day = property(cant_rogue_kill)
 
     def additional_info(self):
         if self.role == Role.objects.get(name__iexact='gay knight'):
@@ -198,10 +198,10 @@ class Player(models.Model):
             else:
                 return "Your Gay Knight partner has not been assigned yet."
         elif self.role == Role.objects.get(name__iexact="rogue"):
-            if not self.rogue_cant_kill:
+            if not self.next_rogue_kill_day:
                 return "You are currently permitted to make kills."
             else:
-                return "You are next allowed to kill on <b> day %d</b>." % self.rogue_cant_kill
+                return "You are next allowed to kill on <b> day %d</b>." % self.next_rogue_kill_day
         elif self.role == Role.objects.get(name__iexact="desperado"):
             if self.role_information == Player.DESPERADO_INACTIVE:
                 return "You have not gone desperado yet."
@@ -293,7 +293,7 @@ class Player(models.Model):
         if self.role == Role.objects.get(name__iexact='mafia') or self.conscripted:
             return True
         elif self.role == Role.objects.get(name__iexact='rogue'):
-            return not self.rogue_cant_kill
+            return not self.next_rogue_kill_day
         elif self.role == Role.objects.get(name__iexact='vigilante'):
             for kill in self.kills.all():
                 if not kill.murderee.is_evil():
@@ -373,7 +373,8 @@ class Player(models.Model):
             links.append((reverse('forms:investigation'), "Make an investigation"))
         if self.role == Role.objects.get(name="Desperado") and self.role_information == Player.DESPERADO_INACTIVE:
             links.append((
-                "javascript:if(confirm('Are you sure you want to go desperado?')==true){window.location.href='%s'}" % reverse(
+                "javascript:if(confirm('Are you sure you want to go desperado?')"
+                "==true){window.location.href='%s'}" % reverse(
                     'go_desperado'), "Go desperado"))
         if self.role == Role.objects.get(name__iexact="Conspiracy theorist"):
             links.append((reverse('forms:conspiracy_list'), 'Update your conspiracy list'))
